996bc2c623b48034eb2d11878265178f20e00a8f
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-05-22 10:12:06 +0200

    Initial Commit

---
+++ Assigment 5/Assigment_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Assigment_5App.swift
+//  Assigment 5
+//
+//  Created by Dennis van den Berg on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assigment_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
